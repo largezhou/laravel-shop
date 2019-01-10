a76c10e8d03a226f313ee5c908b0f5a12ca1311e
largezhou <largezhou@hotmail.com>
2019-01-10 08:20:49 +0800

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,24 +10,19 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
 }
