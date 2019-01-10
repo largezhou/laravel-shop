c343c75593a2b6de33942cb0630cbfc6ef181d48
largezhou <largezhou@hotmail.com>
2019-01-10 23:24:54 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::group([
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
