b91e424e56486f6c60ef1ea03d34c6d1ddd2945d
largezhou <largezhou@hotmail.com>
2019-01-10 21:45:15 +0800

    商品加入购物车

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -70,25 +70,25 @@
 
 @section('scriptsAfterJs')
   <script>
-  $(document).ready(function() {
+  $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip({ trigger: 'hover' })
-    $('.sku-btn').click(function() {
+    $('.sku-btn').click(function () {
       $('.product-info .price span').text($(this).data('price'))
       $('.product-info .stock').text('库存：' + $(this).data('stock') + '件')
     })
 
     // 监听收藏按钮的点击事件
-    $('.btn-favor').click(function() {
+    $('.btn-favor').click(function () {
       // 发起一个 post ajax 请求，请求 url 通过后端的 route() 函数生成。
       axios.post('{{ route('products.favor', ['product' => $product->id]) }}')
         .then(
-          function() { // 请求成功会执行这个回调
+          function () { // 请求成功会执行这个回调
             swal('操作成功', '', 'success')
-              .then(function() {  // 这里加了一个 then() 方法
+              .then(function () {  // 这里加了一个 then() 方法
                 location.reload()
               })
           },
-          function(error) { // 请求失败会执行这个回调
+          function (error) { // 请求失败会执行这个回调
             // 如果返回码是 401 代表没登录
             if (error.response && error.response.status === 401) {
               swal('请先登录', '', 'error')
@@ -103,14 +103,52 @@
         )
     })
 
-    $('.btn-disfavor').click(function() {
+    $('.btn-disfavor').click(function () {
       axios.delete('{{ route('products.disfavor', ['product' => $product->id]) }}')
-        .then(function() {
+        .then(function () {
           swal('操作成功', '', 'success')
-            .then(function() {
+            .then(function () {
               location.reload()
             })
         })
+    })
+
+    // 加入购物车按钮点击事件
+    $('.btn-add-to-cart').click(function () {
+
+      // 请求加入购物车接口
+      axios.post('{{ route('cart.add') }}', {
+        sku_id: $('label.active input[name=skus]').val(),
+        amount: $('.cart_amount input').val(),
+      })
+        .then(
+          function () { // 请求成功执行此回调
+            swal('加入购物车成功', '', 'success')
+          },
+          function (error) { // 请求失败执行此回调
+            if (error.response.status === 401) {
+
+              // http 状态码为 401 代表用户未登陆
+              swal('请先登录', '', 'error')
+
+            } else if (error.response.status === 422) {
+
+              // http 状态码为 422 代表用户输入校验失败
+              var html = '<div>'
+              _.each(error.response.data.errors, function (errors) {
+                _.each(errors, function (error) {
+                  html += error + '<br>'
+                })
+              })
+              html += '</div>'
+              swal({ content: $(html)[0], icon: 'error' })
+            } else {
+
+              // 其他情况应该是系统挂了
+              swal('系统错误', '', 'error')
+            }
+          },
+        )
     })
   })
   </script>
