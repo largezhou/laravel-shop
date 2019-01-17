17a9f102067e8cff4585ce6ee1f533b58a445296
largezhou <largezhou@163.com>
2019-01-17 16:02:10 +0800

    开发环境加入 Ngrok

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -12,3 +12,22 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+/**
+ * 如果是本地环境，并且设置了 ngrok 地址，则返回该 ngrok地址，否则返回 route 函数的地址
+ *
+ * @param string $routeName
+ * @param array  $parameters
+ *
+ * @return string
+ */
+function ngrok_url($routeName, $parameters = [])
+{
+    // 开发环境，并且配置了 NGROK_URL
+    if (app()->environment('local') && $url = config('app.ngrok_url')) {
+        // route() 函数第三个参数代表是否绝对路径
+        return $url.route($routeName, $parameters, false);
+    }
+
+    return route($routeName, $parameters);
+}
