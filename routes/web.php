68ee16339151fc9eafa4d66cc4c9bb7a09f0bb85
largezhou <largezhou@163.com>
2019-01-18 16:22:19 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::group([
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
     Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
