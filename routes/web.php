d2ef03ec8f72ff25e9408c3e33c1d49196c0f7e3
largezhou <largezhou@163.com>
2019-01-22 10:24:42 +0800

    秒杀商品详情页

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,8 @@ Route::group([
     Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
     Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
     Route::get('installments/{installment}/wechat', 'InstallmentsController@payByWechat')->name('installments.wechat');
+
+    Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
