5d886d79465fbecd79f901d10e7cfa532877d7fe
largezhou <largezhou@163.com>
2019-01-22 08:51:18 +0800

    初始化秒杀模型

---
+++ database/migrations/2019_01_22_082706_create_seckill_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeckillProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('seckill_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seckill_products');
+    }
+}
